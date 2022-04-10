d579ac94f079d1ae6d5b0f1afce9ef1a7f92b6d8
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-04-10 11:01:51 +0200

    JS - Stimulus for Hamburger

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import ToggleController from "./toggle_controller.js"
+application.register("toggle", ToggleController)
